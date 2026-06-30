4f87a1726828341726f741cfdce1495c3a33f08e
ncodeinx9000 <66codewarrior99@gmail.com>
2026-06-30 19:52:02 +0530

    first commit

---
+++ basics/c++_basics/input-output.cpp
@@ -0,0 +1,29 @@
+// This is the basic skeleton of the code
+//  #include<iostream> -> Header file
+//  using namespace std; -> Standard namespace
+//  int main(){
+
+//   return 0;
+// }
+
+#include <iostream>
+// #include <math.h>
+// #include <string>
+// #include<bits/stdc++.h> -> it io nclude all the header files mostly used in competitive programming
+using namespace std;
+
+int main()
+{
+  // std::cout << "Hey Nitin!" << std::endl
+  //           << "Hey Nitin!" << std::endl;
+  // std::cout << "Hey Nitin!" << "\n";
+  // std::cout << "Hey Nitin!";
+  // "/n" is preferred beacuse it is faster than "endl"
+
+  // Taking input in c++
+  int x, y;
+  cin >> x >> y;
+  cout << "Value of x: " << x << "and y: " << y;
+
+  return 0;
+}
